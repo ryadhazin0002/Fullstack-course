95db52ff49f5ba9e12960f62967d8af80cc11f43
ryadhazin0002 <159729344+ryadhazin0002@users.noreply.github.com>
2025-04-12 15:02:40 +0200

    comments added to app.js

--- lab1/server/app.js
+++ lab1/server/app.js
@@ -2,15 +2,18 @@ import {app} from './src/express.js'
 import 'dotenv/config.js';
 import DatabaseService from './src/service/databaseService.js';
 
+// Connect to Database
 DatabaseService.connect();
 
-// Start Server
+
 const port = process.env.PORT;
+
+// Start Server
 const server = app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
-
+// Close server and disconnect from database on shutdown
 const shutdown = () => {
   console.log('Shutting down server...');
   server.close(() => {
@@ -20,6 +23,7 @@ const shutdown = () => {
   });
 }
 
+// Handle shutdown signals
 process.on('SIGINT', shutdown)
 process.on('SIGTERM', shutdown)
 
