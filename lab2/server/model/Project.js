d38129a053b279cc251b3c5d830eb6248625a6b8
ryadhazin0002 <159729344+ryadhazin0002@users.noreply.github.com>
2025-04-26 21:55:24 +0200

    fix: add comments to clarify project schema definition and model creation

--- lab2/server/model/Project.js
+++ lab2/server/model/Project.js
@@ -1,9 +1,12 @@
 import mongoose from 'mongoose';
 
+// Define the schema for the project collection
 const projectSchema = new mongoose.Schema({
     project_code: { type: String, required: true, unique: true },
     project_name: { type: String, required: true },
     project_description: { type: String }
 });
 
+
+// Create a model from the schema
 export default mongoose.model('projects', projectSchema);
