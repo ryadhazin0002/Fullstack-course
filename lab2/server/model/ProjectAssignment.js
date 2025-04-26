a1143413e3fbe05648f23a3c108af6121940d2dc
ryadhazin0002 <159729344+ryadhazin0002@users.noreply.github.com>
2025-04-26 21:57:15 +0200

    fix: add comments to clarify project assignment schema and model creation

--- lab2/server/model/ProjectAssignment.js
+++ lab2/server/model/ProjectAssignment.js
@@ -1,9 +1,11 @@
 import mongoose from 'mongoose';
 
+// Define the schema for the projectAssignment collection
 const projectAssignmentSchema = new mongoose.Schema({
     employee: { type: mongoose.Schema.Types.ObjectId, ref: 'employees', required: true},
     project: { type: mongoose.Schema.Types.ObjectId, ref: 'projects', required: true },
     start_date: { type: Date, required: true }
 });
 
+// Create a model from the schema
 export default mongoose.model('projectassignments', projectAssignmentSchema);
